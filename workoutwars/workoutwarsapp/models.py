cec89eaf05e4937c92229fb4d9555d374af2be3e
Sonja Johanson <johanson@berkeley.edu>
2022-12-16 20:18:43 -0800

    added bonus point exercise and updated homepage exercises table

--- workoutwars/workoutwarsapp/models.py
+++ workoutwars/workoutwarsapp/models.py
@@ -88,9 +88,7 @@ class Workout(models.Model):
         i = self.exercise.increment
         m = self.exercise.multiplier
         s = d / i * m
-        print("exercise name:", self.exercise.name)
         if self.exercise.name == 'Biking' or self.exercise.name == 'Running (Distance)' or self.exercise.name == 'Skiing/Snowboarding (Downhill)':
-            print("capping at 5pts!")
             s = min(s, 5) #cap these exercises at 5 points
         #Bonus point for working out with a teammate from another class
         if self.with_other_class:
